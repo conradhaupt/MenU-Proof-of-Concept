4c9ece45fa6a3d195796bb408cfd4b408465365e
Conrad Haupt <me@conradhaupt.co.za>
2013-02-04 10:21:33 +0200

    Started ViewPager on home activity.

--- README.md
+++ README.md
@@ -2,9 +2,4 @@
 ====
 By Conrad Haupt
 -------------------
-
-<<<<<<< HEAD
 This is an app for Android 4.0+ that allows you to order food from a restaurant (mostly for cafeterias) and then collect your food (when notified)through a system of verification methods etc... It is built mainly for use by businesspeople and students who need to order food for later but need to work or they can't get to order as lines are too long.
-=======
-This is an app for Android 4.0+ that allows you to order food from a restaurant (mostly for cafeterias) and then collect your food (when notified)through a system of verification methods etc... It is built mainly for use by businesspeople and students who need to order food for later but need to work or they can't get to order as lines are too long.
->>>>>>> branch 'master' of https://github.com/conradhaupt/MenU.git
--- src/com/conradhaupt/MenU/HomeFragment.java
+++ src/com/conradhaupt/MenU/HomeFragment.java
@@ -1,24 +1,39 @@
 package com.conradhaupt.MenU;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class HomeFragment extends Activity
+public class HomeFragment extends Fragment
 {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState)
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState)
 	{
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.fragment_home);
+		// Inflate the layout for this fragment
+		setHasOptionsMenu(true);
+		super.getActivity().getActionBar().setHomeButtonEnabled(true);
+		return inflater.inflate(R.layout.fragment_home, container, false);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
 	{
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.fragment_home, menu);
+		inflater.inflate(R.menu.fragment_home, menu);
+	}
+
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch (item.getItemId())
+		{
+		default:
+		}
 		return true;
 	}
 
