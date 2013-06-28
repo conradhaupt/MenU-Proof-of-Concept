3cbe1c9b4df1eea30c606db022061e67f62e4b7b
Conrad Haupt <conradjhaupt@gmail.com>
2013-06-28 22:30:40 +0200

    Added the server interaction class with an account creation method that won't work as an account is already created under the hard-coded information. Soon the custom interface will be there so people can begin to make accounts and the login.
    Signed-off-by: Conrad Haupt <conradjhaupt@gmail.com>

--- src/com/conradhaupt/MenU/Core/MenUServerInteraction.java
+++ src/com/conradhaupt/MenU/Core/MenUServerInteraction.java
@@ -3,14 +3,17 @@ package com.conradhaupt.MenU.Core;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicHeader;
-import org.apache.http.protocol.HTTP;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONObject;
 
 import android.util.Log;
@@ -21,25 +24,27 @@ public class MenUServerInteraction
 	{
 		try
 		{
-			JSONObject json = new JSONObject();
-			json.put("username", account.getUsername());
-			json.put("password", account.getPassword());
-			json.put("email", account.getEmail());
-			json.put("firstname", account.getFirstName());
-			json.put("lastname", account.getLastName());
-			String jsonString = "{\"lastname\":\"Haupt\",\"username\":\"Phenominal\",\"firstname\":\"Conrad\",\"email\":\"me@conradhaupt.co.za\",\"password\":\"misteryork\"}";
-			System.out.println(json.toString());
+			List<NameValuePair> variables = new ArrayList<NameValuePair>();
+			variables.add(new BasicNameValuePair("username", account
+					.getUsername()));
+			variables.add(new BasicNameValuePair("password", account
+					.getPassword()));
+			variables.add(new BasicNameValuePair("email", account.getEmail()));
+			variables.add(new BasicNameValuePair("firstname", account
+					.getFirstName()));
+			variables.add(new BasicNameValuePair("lastname", account
+					.getLastName()));
 
 			HttpClient client = new DefaultHttpClient();
 			HttpResponse response;
 			HttpPost post = new HttpPost(
 					"http://conradhaupt.co.za/menuOrderSystem/account_create.php");
-			StringEntity se = new StringEntity("message=" + json.toString());
-			post.addHeader("content-type", "application/json");
-			post.setEntity(se);
+			post.setEntity(new UrlEncodedFormEntity(variables));
 			response = client.execute(post);
+			System.out.println("Executed");
 			if (response != null)
 			{
+				System.out.println("Response doesn't equal null");
 				InputStream in = response.getEntity().getContent();
 
 				String a;
